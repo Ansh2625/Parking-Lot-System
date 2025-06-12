5d03b9323bdcface9aa1b546966b08293be5bd39
Ansh2625 <anshsharma2625@gmail.com>
2025-06-12 19:59:31 +0530

    Added comments

--- Parking-Lot-System/src/vehicle/Vehicle.java
+++ Parking-Lot-System/src/vehicle/Vehicle.java
@@ -5,26 +5,29 @@ public abstract class Vehicle
     private String licensePlate;
     private String vehicleType;
 
+    // Ctor for initialisation
     public Vehicle(String licensePlate, String vehicleType)
     {
         this.licensePlate = licensePlate;
         this.vehicleType = vehicleType;
     }
 
+    // Getters
     public String getLicensePlate()
     {
         return this.licensePlate;
     }
-
     public String getVehicleType()
     {
         return this.vehicleType;
     }
 
+    // Fee Calculation
     public double calculateFee(int hoursStayed)
     {
         return hoursStayed * getRate();
     }
 
+    // abstract function to get Rate accordingly
     protected abstract double getRate();
 }
